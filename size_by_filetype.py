f094205c8bf24393d0b6a3e0c30f17ca8bd058cd
zhrcosta <guilherme.di.aquino@gmail.com>
2021-02-27 21:59:08 -0300

    Primeiro teste OK

---
+++ size_by_filetype.py
@@ -0,0 +1,49 @@
+
+
+def sizeByFileType(path):
+
+    import os
+    import glob
+
+    folder = glob.glob(path + "/*")
+
+    file_list = []
+
+    ext_list = []
+
+    type_count = []
+
+    for file_path in folder:
+        if os.path.isfile(file_path):
+            file_list.append(file_path)
+
+    for file in file_list:
+        file = file.split(".")[-1]
+        ext_list.append(file)
+    
+    for ext in set(ext_list):
+        exttype = ext.upper()
+        amount = 0
+        size = 0
+        
+        for file_path in file_list:
+            if file_path.split(".")[-1] == ext:
+                amount += 1
+                size += os.path.getsize(file_path)
+
+        #print(f'{exttype} ..... {amount} ......{size}')
+        type_count.append((size,amount,exttype))
+    
+    
+    type_count = sorted(type_count)[::-1]
+    
+    for typef in type_count:
+        
+        space_1 = 30 - (len(typef[2]) + len(str(typef[1])))
+        space_2 = 30 - len(str(typef[0]))
+
+        print(f'{typef[2]}{space_1 * "."}{typef[1]}{space_2 * "."}{typef[0]}')
+
+
+
+sizeByFileType('test')
